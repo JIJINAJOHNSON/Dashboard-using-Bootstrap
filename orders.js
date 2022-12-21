f68e4310993a3546b240337ff516212ee31ba7eb
JIJINA JOHNSON <120870616+JIJINAJOHNSON@users.noreply.github.com>
2022-12-21 16:18:24 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,18 @@
+const Orders = [
+    {
+        productName : 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping:'Pending'
+    },
+    {
+        productName : 'Foldable Mini ',
+        productNumber: '85630',
+        paymentStatus: 'Due',
+        shipping:'Declined'
+    }
+]
+    
+
+                           
+                           
